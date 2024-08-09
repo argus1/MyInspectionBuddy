ca0089acf1e6f4a7ae34ee88775c24517ab04fdb
Lorena <ldorado@sandiego.edu>
2024-08-08 18:42:32 -0700

    updated app and db models

--- AddrDb/backend/main.py
+++ AddrDb/backend/main.py
@@ -1,1 +1,28 @@
-# Main route main endpoints
+# Main endpoints/routes for API to access/create resources
+# Define routes needed
+
+# request object and jsonify to return json data
+from flask import request, jsonify
+from config import app, db 
+from models import Contact
+
+# Create: submit request to create endpoint
+# GET context
+# specify route/endpoint in the following decorator
+@app.route("/contacts", methods=["GET"]) # specify valid method type GET method for /contacts url
+def get_contacts(): # function to handle get request that is sent to /contact endpoint
+    contacts = Contact.query.all() # use flask sqlalchemy to get all different contacts in db (non returnable python object)
+    # `contacts` is a list of contact objects (which have the to_json method); call method for all different contacts
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    # map applies lambda function to all elements in the list `contacts` and returns the following new list with json
+    return jsonify({"contacts": json_contacts}) # return json object data instead of python object that says "contacts: json_contacts" 
+    # "contacts" key in python dict; associated with json_contacts (list created)
+    # return python dict object ({"contacts": json_contacts}) converted to json data
+
+
+# run flask application
+if __name__ == "__main__": # if run main.py, then exececute code
+    with app.app_context(): # instantiate db by getting context of app
+        db.create_all() # create all models defined in database only if not created
+    # THEN run the code (app, endpoints, and API)
+    app.run(debug=True) # debug only if main.py ran directly, not if main.py imported
--- AddrDb/backend/models.py
+++ AddrDb/backend/models.py
@@ -5,44 +5,71 @@
 from config import db # relative import from config.py
 from datetime import datetime
 
-# db model as a python class
+# db model (Fields) as a python class
 class Contact(db.Model):
     LicenseAddressId = db.Column(db.Integer, primary_key=True) # unique
-    LicenseId = db.Column(db.Integer, db.ForeignKey('LicenseId')) # unique
-    LicenseNumber = db.Column(db.Integer, db.ForeignKey('LicenseNumber')) # unique
-    LicenseCodeDescription = db.Column(db.String(10), unique=True, nullable=True)
-    ApplicationFormTypeId = db.Column(db.Integer)
-    LicenseTypeId = db.Column(db.Integer)
-    LicenseTypeCode = db.Column(db.String(2), unique=True, nullable=True)
-    LicenseStatusId = db.Column(db.Integer)
-    LicenseStatusCode = db.Column(db.String(2), unique=True, nullable=True) # String
-    LicenseClassificationId = db.Column(db.Datetime, nullable=True, default=datetime.now)
-    LicenseClassificationCode = db.Column(db.String(80), unique=False, nullable=True) 
-    LicenseClassificationDescription = db.Column(db.String(80), unique=False, nullable=True) 
-    ExpirationDate = db.Column(db.Datetime, nullable=True, default=datetime.now)
-    FirmId = db.Column(db.Integer, db.ForeignKey('FirmId')) # unique
-    CorporateName = db.Column(db.String(80), unique=False, nullable=True)
-    BusinessName = db.Column(db.String(80), unique=False, nullable=True)
-    DoingBusinessAs = db.Column(db.String(80), unique=False, nullable=True)
-    StateIncorporation = db.Column(db.String(80), unique=False, nullable=True)
-    AddressLine1 = db.Column(db.String(80), unique=True, nullable=True) # unique
-    AddressLine2 = db.Column(db.String(80), unique=False, nullable=True) 
-    City = db.Column(db.String(80), unique=False, nullable=True) 
-    State = db.Column(db.String(80), unique=False, nullable=True) 
-    Zip = db.Column(db.String(80), unique=False, nullable=True) 
-    CountyId = db.Column(db.Integer)
-    CountyCode = db.Column(db.String(80), unique=False, nullable=True) 
-    LicenseAddressTypeId = db.Column(db.Integer)
-    LicenseAddressTypeCode = db.Column(db.String(80), unique=False, nullable=True) 
-    LicenseAddressTypeDescription = db.Column(db.String(80), unique=False, nullable=True) 
-    ExempteeLastName = db.Column(db.String(80), unique=False, nullable=True) 
-    ExempteeFirstName = db.Column(db.String(80), unique=False, nullable=True) 
+    license_address_id = db.Column(db.Integer, primary_key=True) # unique
+    license_id = db.Column(db.Integer) # unique
+    license_number = db.Column(db.Integer) # unique
+    license_code_description = db.Column(db.String(10), unique=True, nullable=True)
+    application_form_type_id = db.Column(db.Integer)
+    license_type_id = db.Column(db.Integer)
+    license_type_code = db.Column(db.String(2), unique=True, nullable=True)
+    license_status_id = db.Column(db.Integer)
+    license_status_code = db.Column(db.String(2), unique=True, nullable=True) # String
+    # license_classification_id = db.Column(db.Datetime, nullable=True, default=datetime.now)
+    license_classification_code = db.Column(db.String(80), unique=False, nullable=True) 
+    license_classification_description = db.Column(db.String(80), unique=False, nullable=True) 
+    # expiration_date = db.Column(db.Datetime, nullable=True, default=datetime.now)
+    firm_id = db.Column(db.Integer) # unique
+    corporate_name = db.Column(db.String(80), unique=False, nullable=True)
+    business_name = db.Column(db.String(80), unique=False, nullable=True)
+    doing_business_as = db.Column(db.String(80), unique=False, nullable=True)
+    state_incorporation = db.Column(db.String(80), unique=False, nullable=True)
+    address_line_1 = db.Column(db.String(80), unique=True, nullable=True) # unique
+    address_line_2 = db.Column(db.String(80), unique=False, nullable=True) 
+    city = db.Column(db.String(80), unique=False, nullable=True) 
+    state = db.Column(db.String(80), unique=False, nullable=True) 
+    zip = db.Column(db.String(80), unique=False, nullable=True) 
+    county_id = db.Column(db.Integer)
+    county_code = db.Column(db.String(80), unique=False, nullable=True) 
+    license_address_type_id = db.Column(db.Integer)
+    license_address_type_code = db.Column(db.String(80), unique=False, nullable=True) 
+    license_address_type_description = db.Column(db.String(80), unique=False, nullable=True) 
+    exemptee_last_name = db.Column(db.String(80), unique=False, nullable=True) 
+    exemptee_first_name = db.Column(db.String(80), unique=False, nullable=True)
  
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "firstName": self.first_name,
-            "lastName": self.last_name,
-            "email": self.email,
+    # Fields on class object into python dict
+    def to_json(self): # convert to json object (to be passed from API to frontend)
+        return { # return python dict
+            "licenseAddressId": self.license_address_id,
+            "licenseId": self.license_id,
+            "licenseNumber": self.license_number,
+            "licenseCodeDescription": self.license_code_description,
+            "applicationFormTypeId": self.application_form_type_id,
+            "licenseTypeId": self.license_type_id,
+            "licenseTypeCode": self.license_type_code,
+            "licenseStatusId": self.license_status_id,
+            "licenseStatusCode # String": self.license_status_code,
+            # "licenseClassificationId": self.license_classification_id,
+            "licenseClassificationCode": self.license_classification_code,
+            "licenseClassificationDescription": self.license_classification_description,
+            # "expirationDate": self.expiration_date,
+            "firmId": self.firm_id,
+            "corporateName": self.corporate_name,
+            "businessName": self.business_name,
+            "doingBusinessAs": self.doing_business_as,
+            "stateIncorporation": self.state_incorporation,
+            "addressLine1": self.address_line_1,
+            "addressLine2": self.address_line_2,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "countyId": self.county_id,
+            "countyCode": self.county_code,
+            "licenseAddressTypeId": self.license_address_type_id,
+            "licenseAddressTypeCode": self.license_address_type_code,
+            "licenseAddressTypeDescription": self.license_address_type_description,
+            "exempteeLastName": self.exemptee_last_name,
+            "exempteeFirstName": self.exemptee_first_name
         }
